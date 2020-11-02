39c9d340cb90ab8878460f75902cf5f67e3d8517
lorenzOliveto <lorenzo.oliveto@gmail.com>
2020-11-02 17:06:03 +0100

    Addedd comments doc

--- lib/managers/mbaudience_ids_manager.dart
+++ lib/managers/mbaudience_ids_manager.dart
@@ -1,11 +1,18 @@
 import 'package:mbaudience/mbaudience_manager.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// Class used to save and retrieve custom id and mobile user id.
+/// It used the `shared_preferences` package to save them.
 class MBAudienceIdsManager {
+  /// The key used to store the customId
   String _customIdKey = 'com.mumble.mburger.audience.customId';
+  /// The key used to store the mobileUserId
   String _mobileUserIdKey = 'com.mumble.mburger.audience.mobileUserId';
 
   //region custom id
+
+  /// Sets and saves a custom id.
+  /// @param customId The custom id that will be saved.
   Future<void> setCustomId(String customId) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     if (customId != null) {
@@ -16,11 +23,14 @@ class MBAudienceIdsManager {
     await MBAudienceManager.shared.updateMetadata();
   }
 
+  /// Removes the custom id, if present.
   Future<void> removeCustomId() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.remove(_customIdKey);
   }
 
+  /// The custom id, if present, otherwise `null`.
+  /// @returns A Future that completes with the custom id saved.
   Future<String> getCustomId() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String customId = prefs.getString(_customIdKey);
@@ -29,6 +39,8 @@ class MBAudienceIdsManager {
 //endregion
 
 //region mobile user id
+  /// Sets and saves the mobile user id.
+  /// @param mobileUserId The mobile user id that will be saved.
   Future<void> setMobileUserId(int mobileUserId) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     if (mobileUserId != null) {
@@ -39,11 +51,14 @@ class MBAudienceIdsManager {
     await MBAudienceManager.shared.updateMetadata();
   }
 
+  /// Removes the mobile user id, if present.
   Future<void> removeMobileUserId() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.remove(_mobileUserIdKey);
   }
 
+  /// The mobile user id, if present, otherwise `null`.
+  /// @returns A Future that completes with the mobile user id saved.
   Future<int> getMobileUserId() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     int mobileUserId = prefs.getInt(_mobileUserIdKey);
--- lib/managers/mbaudience_session_manager.dart
+++ lib/managers/mbaudience_session_manager.dart
@@ -1,9 +1,12 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// Class that manages MBAudience sessions and session dates.
+/// It uses the `shared_preferences` package to save informations.
 class MBAudienceSessionManager {
   /// The date of start of this session
   DateTime _startSessionDate;
 
+  /// Increases a session for MBAudience.
   Future<void> increaseSession() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     int session = prefs.getInt('com.mumble.mburger.audience.session') ?? 0;
@@ -15,22 +18,29 @@ class MBAudienceSessionManager {
     await startSession();
   }
 
+  /// The key used to store the date of the session with index `session`.
+  /// @param session The session index.
+  /// @return The key to use to save in the shared preference.
   String _sessionDateKeyForSession(int session) {
     String sessionString = session.toString();
     return "com.mumble.mburger.audience.sessionTime.session" + sessionString;
   }
 
+  /// Getter for the current session index.
   Future<int> get currentSession => _currentSession();
 
+  /// A Future that completes with the current session index.
   Future<int> _currentSession() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getInt('com.mumble.mburger.audience.session') ?? 0;
   }
 
+  /// Clears old session values stored.
   Future<void> _clearOldSessionValues() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     int currentSession = await _currentSession();
-    // Clear only the last 3 values, this should be enough because this function will be called at every start, ideally it will find only currentSession - 2
+    //It clears only the last 3 values, this should be enough because this function will be called at every start.
+    //Ideally it will find only currentSession - 2
     List<int> sessionsToClear = [
       currentSession - 2,
       currentSession - 3,
@@ -43,6 +53,7 @@ class MBAudienceSessionManager {
     }
   }
 
+  /// Starts a new session.
   Future<void> startSession() async {
     if (_startSessionDate != null) {
       await endSession();
@@ -50,6 +61,7 @@ class MBAudienceSessionManager {
     _startSessionDate = DateTime.now();
   }
 
+  /// Ends the current session.
   Future<void> endSession() async {
     if (_startSessionDate != null) {
       int sessionTime = await _totalSessionTime();
@@ -60,8 +72,10 @@ class MBAudienceSessionManager {
     }
   }
 
+  /// Getter for the total session time, in seconds.
   Future<int> get totalSessionTime => _totalSessionTime();
 
+  /// Returns the total session time saved, in seconds.
   Future<int> _totalSessionTime() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     int time = prefs.getInt('com.mumble.mburger.audience.sessionTime') ?? 0;
@@ -71,6 +85,9 @@ class MBAudienceSessionManager {
     return time;
   }
 
+  /// The date of start of a session.
+  /// @param session The index of the session.
+  /// @return The date when the session with the index has started, if no session is found this function returns `null`.
   Future<DateTime> startSessionDateForSession(int session) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String key = _sessionDateKeyForSession(session);
